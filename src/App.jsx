4ec908ab0fa029d6e32f6b4bdabb7bc748c51df0
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-10-01 09:14:46 +0200

    Requesting the Data
    In App.jsx create a fetchData function that will handle the fetch requests.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState, useEffect } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
 
 import apiKey from './config'
 
 import Nav from './components/Nav'
-import Photo from './components/Photo'
 import PhotoList from './components/PhotoList'
 import Search from './components/Search'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [photos, setPhotos] = useState({
+    cats: [],
+    dogs: [],
+    computers: [],
+    search: []
+  })
+
+  const fetchData = async (query, topic = 'search') => {
+    try {
+      const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&per_page=24`)
+
+      const data = await response.json()
+      setPhotos(prev => ({
+        ...prev,
+        [topic]: data.hits
+      }));
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+  useEffect(() => {
+    fetchData('cats', 'cats')
+    fetchData('dogs', 'dogs')
+    fetchData('computers', 'computers')
+  }, [])
 
   return (
     <>
       <div className="container">
-        <Search />
+        <Search fetchData={fetchData}/>
         <Nav />
 
         <Routes>
            <Route path="/" element={<Navigate to="/cats" />} />
 
-           <Route path="/cats" element={<PhotoList topic="cats" />} />
-            <Route path="/dogs" element={<PhotoList topic="dogs" />} />
-            <Route path="/computers" element={<PhotoList topic="computers" />} />
+           <Route path="/cats" element={<PhotoList photos={photos.cats} topic="cats" />} />
+            <Route path="/dogs" element={<PhotoList photos={photos.dogs} topic="dogs" />} />
+            <Route path="/computers" element={<PhotoList photos={photos.computers} topic="computers" />} />
 
-            <Route path="/search/:query" element={<PhotoList />} />
+            <Route path="/search/:query" element={<PhotoList photos={photos.search} />} />
         </Routes>
       </div>
     </>
